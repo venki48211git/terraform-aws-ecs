a00bb6422b61c1d83421f632567166df8540c430
Venkateshwaran Senathipathi <73068630+venki48211git@users.noreply.github.com>
2022-07-29 02:03:35 +0530

    commit

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "tags" {
 variable "cluster_name" {
   description = "Name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
   type        = string
-  default     = ""
+  default     = "venkatCl1"
 }
 
 variable "cluster_configuration" {
